232dc8af35718e5bf34c208532b4ed247cdc5757
Berkant <berki9@icloud.com>
2023-07-14 20:48:00 +0300

    add active page js

--- src/main/java/com/example/LukeriaFrontendApplication/controllers/Controller.java
+++ src/main/java/com/example/LukeriaFrontendApplication/controllers/Controller.java
@@ -3,7 +3,6 @@ package com.example.LukeriaFrontendApplication.controllers;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 @org.springframework.stereotype.Controller
 @RequiredArgsConstructor
 @Slf4j
