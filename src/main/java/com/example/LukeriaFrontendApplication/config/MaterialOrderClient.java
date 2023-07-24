a00e8d170c0ed875eee0f72f7ad8e2fd9ad06e2f
BelisAliosmanova <107267662+BelisAliosmanova@users.noreply.github.com>
2023-07-24 17:03:11 +0300

    add page for displaying all material orders add delete option changes in header

--- src/main/java/com/example/LukeriaFrontendApplication/config/MaterialOrderClient.java
+++ src/main/java/com/example/LukeriaFrontendApplication/config/MaterialOrderClient.java
@@ -11,19 +11,19 @@ import java.util.List;
 @FeignClient(name = "lukeria-erp-material-order", url = "http://localhost:8088/api/v1/material-order")
 public interface MaterialOrderClient {
     @GetMapping
-    ResponseEntity<List<MaterialOrderDTO>> getAllMaterialOrders();
+    List<MaterialOrderDTO> getAllMaterialOrders();
 
     @GetMapping("/{id}")
-    ResponseEntity<MaterialOrderDTO> getMaterialOrderById(@PathVariable(name = "id") Long id);
+    MaterialOrderDTO getMaterialOrderById(@PathVariable(name = "id") Long id);
 
     @PostMapping
-    ResponseEntity<MaterialOrderDTO> createMaterialOrder(@Valid @RequestBody MaterialOrderDTO materialOrderDTO);
+    MaterialOrderDTO createMaterialOrder(@Valid @RequestBody MaterialOrderDTO materialOrderDTO);
 
     @PutMapping("/{id}")
-    ResponseEntity<MaterialOrderDTO> updateMaterialOrder(@PathVariable("id") Long id, @Valid @RequestBody MaterialOrderDTO materialOrderDTO);
+    MaterialOrderDTO updateMaterialOrder(@PathVariable("id") Long id, @Valid @RequestBody MaterialOrderDTO materialOrderDTO);
 
     @DeleteMapping("/{id}")
-    ResponseEntity<String> deleteMaterialOrderById(@PathVariable("id") Long id);
+    String deleteMaterialOrderById(@PathVariable("id") Long id);
 
     @GetMapping("products/{id}")
     void getAllProductsByOrderId(@PathVariable(name = "id") Long id);
--- src/main/java/com/example/LukeriaFrontendApplication/controllers/MaterialOrderController.java
+++ src/main/java/com/example/LukeriaFrontendApplication/controllers/MaterialOrderController.java
@@ -1,7 +1,10 @@
 package com.example.LukeriaFrontendApplication.controllers;
 
 import com.example.LukeriaFrontendApplication.config.*;
+import com.example.LukeriaFrontendApplication.dtos.CartonDTO;
+import com.example.LukeriaFrontendApplication.dtos.ClientDTO;
 import com.example.LukeriaFrontendApplication.dtos.MaterialOrderDTO;
+import com.example.LukeriaFrontendApplication.dtos.OrderDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ui.Model;
@@ -32,10 +35,30 @@ public class MaterialOrderController {
         model.addAttribute(ORDERTXT, materialOrderDTO);
         return "MaterialOrder/create";
     }
+    @GetMapping("/show")
+    public String index(Model model) {
+        List<MaterialOrderDTO> materialOrderDTOS = materialOrderClient.getAllMaterialOrders();;
+        model.addAttribute("orders", materialOrderDTOS);
+        return "MaterialOrder/show";
+    }
 
     @PostMapping("/submit")
     public ModelAndView submitMaterialOrder(@ModelAttribute("order") MaterialOrderDTO materialOrderDTO) {
         materialOrderClient.createMaterialOrder(materialOrderDTO);
         return new ModelAndView("redirect:/index");
+    }
+    @PostMapping("/delete/{id}")
+    ModelAndView deleteMaterialOrderById(@PathVariable("id") Long id, Model model) {
+        materialOrderClient.deleteMaterialOrderById(id);
+        return new ModelAndView(REDIRECTTXT);
+    }
+    @GetMapping("/edit/{id}")
+    String editMaterialOrder(@PathVariable(name = "id") Long id, Model model) {
+        MaterialOrderDTO existingOrder = materialOrderClient.getMaterialOrderById(id);
+        model.addAttribute("cartons", cartonClient.getAllCartons());
+        model.addAttribute("packages", packageClient.getAllPackages());
+        model.addAttribute("plates", plateClient.getAllPlates());
+        model.addAttribute(ORDERTXT, existingOrder);
+        return "OrderProduct/edit";
     }
 }
