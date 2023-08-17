13a1253a36341d7385d3c88a38b598b6166480ef
BelisAliosmanova <107267662+BelisAliosmanova@users.noreply.github.com>
2023-08-17 17:09:50 +0300

    upload images

--- src/main/java/com/example/LukeriaFrontendApplication/controllers/PackageController.java
+++ src/main/java/com/example/LukeriaFrontendApplication/controllers/PackageController.java
@@ -1,5 +1,6 @@
 package com.example.LukeriaFrontendApplication.controllers;
 
+
 import com.example.LukeriaFrontendApplication.config.CartonClient;
 import com.example.LukeriaFrontendApplication.config.PackageClient;
 import com.example.LukeriaFrontendApplication.config.PlateClient;
@@ -8,11 +9,10 @@ import com.example.LukeriaFrontendApplication.dtos.PackageDTO;
 import com.example.LukeriaFrontendApplication.dtos.PlateDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -24,6 +24,7 @@ public class PackageController {
     private final PackageClient packageClient;
     private final CartonClient cartonClient;
     private final PlateClient plateClient;
+    private final UploadController uploadController;
     private static final String REDIRECTTXT ="redirect:/package/show";
 
 
@@ -46,11 +47,11 @@ public class PackageController {
     }
 
     @PostMapping("/package/submit")
-    public ModelAndView submitPackage(@ModelAttribute("packageEntity") PackageDTO packageDTO) {
+    public ModelAndView submitPackage(@ModelAttribute("packageEntity") PackageDTO packageDTO, @RequestParam MultipartFile file) {
+        packageDTO.setPhoto(file);
         packageClient.createPackage(packageDTO);
         return new ModelAndView(REDIRECTTXT);
     }
-
     @GetMapping("/package/editPackage/{id}")
     String editPackage(@PathVariable(name = "id") Long id, Model model) {
         PackageDTO existingPackage = packageClient.getPackageById(id);
