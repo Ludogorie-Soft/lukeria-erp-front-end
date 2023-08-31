668b0fdc40c6f399dcd860f21d7e235bae6e78d6
BelisAliosmanova <107267662+BelisAliosmanova@users.noreply.github.com>
2023-08-31 15:38:49 +0300

    image storing for plates crud -delete no needed dto-s

--- src/main/java/com/example/LukeriaFrontendApplication/controllers/PlateController.java
+++ src/main/java/com/example/LukeriaFrontendApplication/controllers/PlateController.java
@@ -1,13 +1,18 @@
 package com.example.LukeriaFrontendApplication.controllers;
 
+import com.example.LukeriaFrontendApplication.config.ImageClient;
 import com.example.LukeriaFrontendApplication.config.PlateClient;
+import com.example.LukeriaFrontendApplication.dtos.PackageDTO;
 import com.example.LukeriaFrontendApplication.dtos.PlateDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.IOException;
 import java.util.List;
 
 @org.springframework.stereotype.Controller
@@ -16,12 +21,21 @@ import java.util.List;
 @RequestMapping("/plate")
 public class PlateController {
     private final PlateClient plateClient;
+    private final ImageClient imageService;
+    @Value("${backend.base-url}")
+    private String backendBaseUrl;
     private  static final String CARTONTXT = "plate";
     private static final String REDIRECTTXT = "redirect:/plate/show";
 
     @GetMapping("/show")
     public String index(Model model) {
         List<PlateDTO> plates = plateClient.getAllPlates();
+        for (PlateDTO plate : plates) {
+            if(plate.getPhoto()!=null) {
+                imageService.getImage(plate.getPhoto());
+            }
+        }
+        model.addAttribute("backendBaseUrl", backendBaseUrl);
         model.addAttribute("deleteMessageBoolean", true);
         model.addAttribute("plates", plates);
         return "Plate/show";
@@ -48,14 +62,20 @@ public class PlateController {
         model.addAttribute(CARTONTXT, existingPlate);
         return "Plate/edit";
     }
-    @GetMapping("/edit/{id}")
-    ModelAndView editPlate(@PathVariable(name = "id") Long id, PlateDTO plateDTO) {
+    @PostMapping("/edit/{id}")
+    ModelAndView editPlate(@PathVariable(name = "id") Long id, PlateDTO plateDTO, @RequestParam("file") MultipartFile file) throws IOException {
         plateClient.updatePlate(id, plateDTO);
+        if (!file.isEmpty()) {
+            imageService.editImageForPlate(file, id);
+        }
         return new ModelAndView(REDIRECTTXT);
     }
     @PostMapping("/submit")
-    public ModelAndView submitPlate(@ModelAttribute("plate") PlateDTO plateDTO) {
+    public ModelAndView submitPlate(@ModelAttribute("plate") PlateDTO plateDTO, @RequestParam MultipartFile file) throws IOException {
         plateClient.createPlate(plateDTO);
+        if (!file.isEmpty()) {
+            imageService.uploadImageForPlate(file);
+        }
         return new ModelAndView(REDIRECTTXT);
     }
     @PostMapping("/delete/{id}")
