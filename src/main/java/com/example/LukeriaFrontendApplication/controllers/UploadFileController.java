fb889595f36e803494db1ecfcef5663d2fb6c557
BelisAliosmanova <107267662+BelisAliosmanova@users.noreply.github.com>
2023-12-22 20:29:45 +0200

    front-end upload file

---
+++ src/main/java/com/example/LukeriaFrontendApplication/controllers/UploadFileController.java
@@ -0,0 +1,32 @@
+package com.example.LukeriaFrontendApplication.controllers;
+
+import com.example.LukeriaFrontendApplication.config.UploadFileClient;
+import com.example.LukeriaFrontendApplication.dtos.PackageDTO;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
+
+@org.springframework.stereotype.Controller
+@RequiredArgsConstructor
+@Slf4j
+public class UploadFileController {
+    private final UploadFileClient uploadFileClient;
+    @GetMapping("/upload/file")
+    String createPackage(Model model, HttpServletRequest request) {
+        return "uploadFile";
+    }
+    @PostMapping("/upload/file/submit")
+    public ModelAndView uploadFromFile(@RequestParam MultipartFile file) throws IOException {
+        uploadFileClient.uploadFromFile(file);
+        return new ModelAndView("redirect:/package/show");
+    }
+}
