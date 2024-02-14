62ff26c5a78b93c7b93cd95e34639390717d9553
Valyo96 <107226915+Valyo96@users.noreply.github.com>
2024-02-14 16:54:27 +0200

    Add slack integration for error notifications

--- src/main/java/com/example/LukeriaFrontendApplication/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/LukeriaFrontendApplication/exception/GlobalExceptionHandler.java
@@ -1,14 +1,15 @@
 package com.example.LukeriaFrontendApplication.exception;
 
 import com.example.LukeriaFrontendApplication.slack.SlackService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
+@RequiredArgsConstructor
 public class GlobalExceptionHandler {
-    @Autowired
-    private SlackService slackService;
+
+    private final SlackService slackService;
 
     @ExceptionHandler(Exception.class)
     public void alertSlackChannelWhenUnexpectedErrorOccurs(Exception ex) {
