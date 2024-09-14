1523c212cebd2050b90b12b4854feeb2a095cc51
MirkaanStefanov <135588213+MirkaaStefanov@users.noreply.github.com>
2024-09-14 04:32:28 +0300

    Add form to create custom price for customer

--- src/main/java/com/example/LukeriaFrontendApplication/dtos/CustomerCustomPriceDTO.java
+++ src/main/java/com/example/LukeriaFrontendApplication/dtos/CustomerCustomPriceDTO.java
@@ -1,9 +1,15 @@
 package com.example.LukeriaFrontendApplication.dtos;
 
 import jakarta.validation.constraints.Min;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CustomerCustomPriceDTO {
 
     private Long id;
