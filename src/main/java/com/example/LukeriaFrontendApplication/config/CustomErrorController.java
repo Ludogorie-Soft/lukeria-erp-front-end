1fb9d53b1fc562b83b4329e661f8d1b08a3ed064
berki0 <107307903+berki0@users.noreply.github.com>
2024-09-14 19:39:18 +0300

    Add ApiExceptionHandler in BE

--- src/main/java/com/example/LukeriaFrontendApplication/config/CustomErrorController.java
+++ src/main/java/com/example/LukeriaFrontendApplication/config/CustomErrorController.java
@@ -1,14 +1,22 @@
 package com.example.LukeriaFrontendApplication.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CustomErrorController implements ErrorController {
 
     @RequestMapping("/error")
-    public String handleError() {
+    public String handleError(HttpServletRequest request, Model model) {
+        Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+        String errorMessage = (String) request.getAttribute("javax.servlet.error.message");
+
+        model.addAttribute("statusCode", statusCode);
+        model.addAttribute("errorMessage", errorMessage != null ? errorMessage : "An unexpected error occurred");
+
         return "error";
     }
 }
