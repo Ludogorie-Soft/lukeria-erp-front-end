a64f2924dcc1dfde1043af3812dd921ad027a168
MirkaanStefanov <135588213+MirkaaStefanov@users.noreply.github.com>
2024-09-19 12:24:36 +0300

    showing custom prices for client, update, delete done. Changed creating

--- src/main/java/com/example/LukeriaFrontendApplication/config/CustomerCustomPriceClient.java
+++ src/main/java/com/example/LukeriaFrontendApplication/config/CustomerCustomPriceClient.java
@@ -1,6 +1,7 @@
 package com.example.LukeriaFrontendApplication.config;
 
 import com.example.LukeriaFrontendApplication.dtos.CustomerCustomPriceDTO;
+import com.example.LukeriaFrontendApplication.dtos.ProductDTO;
 import jakarta.validation.Valid;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
@@ -11,8 +12,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Map;
 
 @FeignClient(name = "lukeria-erp-customerCustomPrice", url = "${backend.base-url}/customerCustomPrice")
 public interface CustomerCustomPriceClient {
@@ -23,10 +27,13 @@ public interface CustomerCustomPriceClient {
     CustomerCustomPriceDTO createCustomPriceForCustomer(@Valid @RequestBody CustomerCustomPriceDTO customerCustomPriceDTO, @RequestHeader("Authorization") String auth);
 
     @PutMapping("/{id}")
-    CustomerCustomPriceDTO updateCustomPrice(@Valid @RequestBody CustomerCustomPriceDTO customerCustomPriceDTO, @PathVariable(name = "id") Long id, @RequestHeader("Authorization") String auth);
+    CustomerCustomPriceDTO updateCustomPrice(@Valid @RequestBody CustomerCustomPriceDTO customerCustomPriceDTO, @RequestHeader("Authorization") String auth);
 
-    @DeleteMapping("/{id}")
-    CustomerCustomPriceDTO deleteCustomPrice(@PathVariable(name = "id") Long id, @RequestHeader("Authorization") String auth);
+    @DeleteMapping("/")
+    CustomerCustomPriceDTO deleteCustomPrice(@RequestParam(name = "clientId") Long clientId, @RequestParam(name = "productId") Long productId, @RequestHeader("Authorization") String auth);
+
+    @GetMapping("/allForClient/{id}")
+    List<CustomerCustomPriceDTO> allProductsWithAndWithoutCustomPrice(@PathVariable(name = "id") Long clientId, @RequestHeader("Authorization") String auth);
 
 }
 
--- src/main/java/com/example/LukeriaFrontendApplication/dtos/ProductDTO.java
+++ src/main/java/com/example/LukeriaFrontendApplication/dtos/ProductDTO.java
@@ -1,16 +1,24 @@
 package com.example.LukeriaFrontendApplication.dtos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
+@JsonFormat
 public class ProductDTO {
     private Long id;
     @Min(value = 1, message = "Цената не може да бъде отрицателно число!")
