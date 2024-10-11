8a526802931eff01e16fbc25d9b3f5742b0b499e
MirkaanStefanov <135588213+MirkaaStefanov@users.noreply.github.com>
2024-10-11 09:08:52 +0300

    Create CartItemDTO

---
+++ src/main/java/com/example/LukeriaFrontendApplication/dtos/CartItemDTO.java
@@ -0,0 +1,15 @@
+package com.example.LukeriaFrontendApplication.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CartItemDTO {
+
+    private Long id;
+    private Long productId;
+    private double price;
+}
