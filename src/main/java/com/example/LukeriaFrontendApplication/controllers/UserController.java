4118764135f7f913ee544311bd0dc22207c2b400
Eri1905 <ery.mtl1905@gmail.com>
2024-11-04 01:01:36 +0200

    All done

--- src/main/java/com/example/LukeriaFrontendApplication/controllers/UserController.java
+++ src/main/java/com/example/LukeriaFrontendApplication/controllers/UserController.java
@@ -11,12 +11,16 @@ import com.example.LukeriaFrontendApplication.enums.Role;
 import com.example.LukeriaFrontendApplication.models.User;
 import com.example.LukeriaFrontendApplication.session.SessionManager;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.ValidationException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -60,7 +64,7 @@ public class UserController {
     }
 
     @GetMapping("/create")
-    String createUser(Model model, HttpServletRequest request) {
+    String createUser(Model model, HttpServletRequest request, @ModelAttribute("emailAlreadyExists") String message) {
         String token = (String) request.getSession().getAttribute(SESSION_TOKEN);
 
         List<ClientDTO> clients = clientUserClient.getAllClientWithNoUser(token);
@@ -69,17 +73,25 @@ public class UserController {
 
         model.addAttribute("clientsForSelect", clients);
         model.addAttribute("user", user);
+        model.addAttribute("emailAlreadyExists", message);
         return "User/create";
     }
 
     @PostMapping("/submit")
-    public ModelAndView submitUser(@ModelAttribute("user") UserDTO user, HttpServletRequest request) {
+    public ModelAndView submitUser(@ModelAttribute("user") UserDTO user, HttpServletRequest request, Model model) {
         String token = (String) request.getSession().getAttribute(SESSION_TOKEN);
-        UserDTO createdUser = userClient.createUser(user, token);
-
-        if (user.getClientID() != null) {
-            ClientUserDTO clientUserDTO = new ClientUserDTO(null, user.getClientID(), createdUser.getId());
-            clientUserClient.createClientUser(clientUserDTO, token);
+        List<ClientDTO> clients = clientUserClient.getAllClientWithNoUser(token);
+        try {
+            UserDTO createdUser = userClient.createUser(user, token);
+            if (user.getClientID() != null) {
+                ClientUserDTO clientUserDTO = new ClientUserDTO(null, user.getClientID(), createdUser.getId());
+                clientUserClient.createClientUser(clientUserDTO, token);
+            }
+        } catch (Exception ex) {
+            model.addAttribute("user", user);
+            model.addAttribute("clientsForSelect", clients);
+            model.addAttribute("emailAlreadyExists", "Потребител с този имейл вече съществува!");
+            return new ModelAndView("User/create");
         }
         return new ModelAndView(REDIRECTTXT);
     }
@@ -92,7 +104,7 @@ public class UserController {
 
         UserDTO authenticatedUser = userClient.findAuthenticatedUser(token);
 
-        if (authenticatedUser.getRole().equals(Role.ADMIN)){
+        if (authenticatedUser.getRole().equals(Role.ADMIN)) {
             List<ClientDTO> clients = clientUserClient.getAllClientWithNoUser(token);
             model.addAttribute("clientsForSelect", clients);
         }
@@ -107,18 +119,27 @@ public class UserController {
     @PostMapping("/edit/{id}")
     ModelAndView editSubmitUser(@PathVariable(name = "id") Long id, UserDTO userDTO, HttpServletRequest request) {
         String token = (String) request.getSession().getAttribute(SESSION_TOKEN);
-        UserDTO authenticatedUser = userClient.findAuthenticatedUser(token);
-        UserDTO existingUser = userClient.getUserById(id, token);
+        try {
+            UserDTO authenticatedUser = userClient.findAuthenticatedUser(token);
+            UserDTO existingUser = userClient.getUserById(id, token);
 
-        if (userDTO.getClientID() != null) {
-            clientUserClient.createClientUser(new ClientUserDTO(null, userDTO.getClientID(), userDTO.getId()), token);
+            if (userDTO.getClientID() != null) {
+                clientUserClient.createClientUser(new ClientUserDTO(null, userDTO.getClientID(), userDTO.getId()), token);
+            }
+
+            if (existingUser.equals(authenticatedUser)) {
+                AuthenticationResponse authenticationResponse = userClient.updateAuthenticatedUser(id, userDTO, token);
+                sessionManager.setSessionToken(request, authenticationResponse.getAccessToken(), authenticationResponse.getUser().getRole().toString());
+                return new ModelAndView(REDIRECTTXT2);
+            }
+            userClient.updateUser(id, userDTO, token);
+        } catch (Exception ex) {
+            ModelAndView modelAndView = new ModelAndView("redirect:/user/editUser/" + userDTO.getId());
+            modelAndView.addObject(ERRORTXT, "Потребител с този имейл вече съществува!");
+            return modelAndView;
         }
-        if (existingUser.equals(authenticatedUser)) {
-            AuthenticationResponse authenticationResponse = userClient.updateAuthenticatedUser(id, userDTO, token);
-            sessionManager.setSessionToken(request, authenticationResponse.getAccessToken(), authenticationResponse.getUser().getRole().toString());
-            return new ModelAndView(REDIRECTTXT2);
-        }
-        userClient.updateUser(id, userDTO, token);
+
+
         return new ModelAndView(REDIRECTTXT);
     }
 
