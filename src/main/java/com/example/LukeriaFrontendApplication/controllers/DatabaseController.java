8b6c8465dba75453bb190ea90f9bd962ec116675
berki0 <107307903+berki0@users.noreply.github.com>
2025-01-12 16:03:07 +0200

    add update method

--- src/main/java/com/example/LukeriaFrontendApplication/controllers/DatabaseController.java
+++ src/main/java/com/example/LukeriaFrontendApplication/controllers/DatabaseController.java
@@ -2,12 +2,14 @@ package com.example.LukeriaFrontendApplication.controllers;
 
 import com.example.LukeriaFrontendApplication.config.*;
 import com.example.LukeriaFrontendApplication.dtos.*;
+import com.example.LukeriaFrontendApplication.models.User;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.*;
 
@@ -23,16 +25,14 @@ public class DatabaseController {
     private final ProductClient productClient;
     private final PlateClient plateClient;
     private final ClientClient clientClient;
+    private final OrderClient orderClient;
+    private final OrderProductClient orderProductClient;
     private final CustomerCustomPriceClient customerCustomPriceClient;
     private final InvoiceClient invoiceClient;
     private final InvoiceOrderProductClient invoiceOrderProductClient;
     private final MaterialOrderClient materialOrderClient;
     private final MonthlyOrderClient monthlyOrderClient;
     private final MonthlyOrderProductClient monthlyOrderProductClient;
-    private final OrderClient orderClient;
-    private final OrderProductClient orderProductClient;
-    private final QueryClient queryClient;
-    private final ShoppingCartClient shoppingCartClient;
     private final UserClient userClient;
 
     @GetMapping("/show")
@@ -41,12 +41,24 @@ public class DatabaseController {
         List<PackageDTO> packages = packageClient.getAllPackages(token);
         List<PlateDTO> plates = plateClient.getAllPlates(token);
         List<CartonDTO> cartons = cartonClient.getAllCartons(token);
-        List<ProductDTO> productDTOS =productClient.getAllProducts(token);
-        List<ClientDTO> clientDTOS=clientClient.getAllClients(token);
-        List<OrderDTO> orderDTOS=orderClient.getAllOrders(token);
+        List<ProductDTO> productDTOS = productClient.getAllProducts(token);
+        List<ClientDTO> clientDTOS = clientClient.getAllClients(token);
+        List<OrderDTO> orderDTOS = orderClient.getAllOrders(token);
         List<OrderProductDTO> orderProductDTOS = orderProductClient.getAllOrderProducts(token);
-
-
+        List<InvoiceDTO> invoiceDTOS = invoiceClient.getAllInvoices(token);
+        List<CustomerCustomPriceDTO> customerCustomPriceDTOS = customerCustomPriceClient.getAllCustomPrices(token);
+        List<InvoiceOrderProductDTO> invoiceOrderProductDTOS = invoiceOrderProductClient.getAllInvoiceOrderProduct(token);
+        List<MaterialOrderDTO> materialOrderDTOS = materialOrderClient.getAllMaterialOrders(token);
+        List<MonthlyOrderDTO> monthlyOrderDTOS = monthlyOrderClient.getAllMonthlyOrders(token);
+        List<MonthlyOrderProductDTO> monthlyOrderProductDTOS = monthlyOrderProductClient.getAllMonthlyProductOrders(token);
+        List<UserDTO> userDTOS = userClient.getAllUsers(token);
+        model.addAttribute("monthlyOrderProductDTOS", monthlyOrderProductDTOS);
+        model.addAttribute("monthlyOrderDTOS", monthlyOrderDTOS);
+        model.addAttribute("userDTOS", userDTOS);
+        model.addAttribute("materialOrderDTOS", materialOrderDTOS);
+        model.addAttribute("invoiceOrderProductDTOS", invoiceOrderProductDTOS);
+        model.addAttribute("customerCustomPriceDTOS", customerCustomPriceDTOS);
+        model.addAttribute("invoiceDTOS", invoiceDTOS);
         model.addAttribute("orderDTOS", orderDTOS);
         model.addAttribute("orderProductDTOS", orderProductDTOS);
         model.addAttribute("clientDTOS", clientDTOS);
@@ -56,4 +68,16 @@ public class DatabaseController {
         model.addAttribute("packages", packages);
         return "Database/show";
     }
+
+    @PostMapping("/edit/{id}")
+    public ModelAndView editObjects(@PathVariable(name = "id") Long id,
+                                    @ModelAttribute OrderProductDTO object,
+                                    HttpServletRequest request) {
+
+        String token = (String) request.getSession().getAttribute(SESSION_TOKEN);
+        orderProductClient.updateOrderProduct(id, object, token);
+
+        return new ModelAndView(REDIRECTTXT);
+    }
+
 }
