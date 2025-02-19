5c2be1e93d1a9f213653b1608375b86f31e2ccdc
berki0 <107307903+berki0@users.noreply.github.com>
2025-02-19 20:43:26 +0200

    page improvement

--- src/main/java/com/example/LukeriaFrontendApplication/config/ManufacturedProductsClient.java
+++ src/main/java/com/example/LukeriaFrontendApplication/config/ManufacturedProductsClient.java
@@ -2,7 +2,6 @@ package com.example.LukeriaFrontendApplication.config;
 
 import com.example.LukeriaFrontendApplication.dtos.ManufacturedProductDTO;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
@@ -10,7 +9,7 @@ import java.util.List;
 
 @FeignClient(name = "lukeria-erp-manufactured-products", url = "${backend.base-url}/manufactured-product")
 public interface ManufacturedProductsClient {
-    @GetMapping("/all")
+    @GetMapping
     List<ManufacturedProductDTO> getAllManufacturedProducts(@RequestHeader("Authorization") String auth);
 
     }
